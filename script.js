2cfabfff574fafe9460ff5b7db8c78eb6daef92b
Rajeev Pandey <rajeevpandey.devac@gmail.com>
2022-03-27 12:58:32 +0530

    fixed issues with the overlay

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ console.log(linkList);
 hamburgerIcon.addEventListener("click",()=>{
     hamburgerIcon.classList.add("hamburger-icon-container-hidden");
     arrowIcon.classList.add("arrow-icon-animation-class");
+    overlay.classList.remove("overlay-fill-reverse-animation-class");
+    overlay.classList.remove("overlay-fill-reverse-animation-class-mobile");
     overlay.classList.add("overlay-fill-animation-class");
     linkList.classList.add("link-list");
     linkList.classList.remove("link-list-hidden");
@@ -17,6 +19,13 @@ arrowIcon.addEventListener("click",()=>{
     arrowIcon.classList.remove("arrow-icon-animation-class");
     arrowIcon.classList.add("arrow-icon-reverse-animation-class");
     overlay.classList.remove("overlay-fill-animation-class");
+
+    if(document.body.clientWidth<=500){
+        overlay.classList.add("overlay-fill-reverse-animation-class-mobile");
+    }else{
+        overlay.classList.add("overlay-fill-reverse-animation-class");
+    }
+    
     linkList.classList.remove("link-list");
     linkList.classList.add("link-list-hidden");
     setTimeout(()=>{
